cc74ac4fe3afc250acb10f6d2d50976128e1d71a
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-02-25 00:19:01 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        sc.close();
+
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial: " + fact);
+    }
+}
